615e7bfa44f2e8bb31ecf0419d44860879f27e2b
diego zarate <diegoazarate@hotmail.com>
2023-02-24 06:39:33 -0300

    dia 2 bonus + update day2

---
+++ bonus/bonus2.1.py
@@ -0,0 +1,10 @@
+
+
+password = input("input a password: ")
+
+
+while password != "pass123":
+    password = input("input a password: ")
+
+
+print("password was correct!")
--- day2.py
+++ day2.py
@@ -1,14 +1,16 @@
 
 
-user_promt = "Enter a todo: "
 
+user_promt = "Enter a todo: "
 todos = []
 
 while True:
     todo = input(user_promt)
     todos.append(todo)
     print(todo.capitalize())
+    print(todo.title())
     print(todos)
+
 
 
     print("next")
