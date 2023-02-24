f27a92a766558ecfbd37120674c044d1481eafe7
diego zarate <diegoazarate@hotmail.com>
2023-02-24 07:07:57 -0300

    dia 3

---
+++ day3.py
@@ -0,0 +1,17 @@
+
+todos = []
+
+while True:
+    user_action = input("Type 1 for add or 2 for show or 3 for exit: ")
+
+    match user_action:
+        case "1":
+            todo = input("enter a todo :")
+            todos.append(todo)
+        case "2":
+            print(todos)
+        case "3":
+            break
+
+
+print("Bye!!!!!!!")
