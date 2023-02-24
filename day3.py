6bc93951b29e40562bd856772c42f14b6322c352
diego zarate <diegoazarate@hotmail.com>
2023-02-24 11:25:03 -0300

    bonus

--- day3.py
+++ day3.py
@@ -8,14 +8,15 @@ while True:
     user_action = user_action.strip()
 
     match user_action:
-        case "1":
+        case "1" | "add" :
             todo = input("enter a todo :")
             todos.append(todo)
-        case "2":
+        case "2" | "show" :
             print(todos)
             for item in todos:
+                item = item.title()
                 print(item)
-        case "3":
+        case "3" | "exit" :
             break
         case whatever :
             print("what da fuck?????")
