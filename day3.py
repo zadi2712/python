4de827b04fd788fc9dc633cb1f6dab1bdeb985e1
diego zarate <diegoazarate@hotmail.com>
2023-02-24 07:16:50 -0300

    dia 3 add a for loop

--- day3.py
+++ day3.py
@@ -1,8 +1,12 @@
+import os
 
 todos = []
 
+
 while True:
     user_action = input("Type 1 for add or 2 for show or 3 for exit: ")
+
+
 
     match user_action:
         case "1":
@@ -10,6 +14,8 @@ while True:
             todos.append(todo)
         case "2":
             print(todos)
+            for item in todos:
+                print(item)
         case "3":
             break
 
