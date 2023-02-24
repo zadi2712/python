bf5c7ac47fa1d166cf77336d254b823d80b57861
diego zarate <diegoazarate@hotmail.com>
2023-02-24 05:17:09 -0300

    dia 1

---
+++ main.py
@@ -0,0 +1,14 @@
+user_promt = "enter a todo:"
+
+todo1 = input(user_promt)
+todo2 = input(user_promt)
+todo3 = input(user_promt)
+
+todos = [todo1, todo2, todo3]
+
+print(todos)
+
+length = len(user_promt)
+print(length)
+
+print(type(todos))
